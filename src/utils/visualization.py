2a9b0f5ad956911fa6b85f4c9c466f1c0cb5402b
Jannik Wiedenhaupt <ecbm4040@jjw2196-instance-1.c.ecbm4040-jjw2196.internal>
2021-12-19 22:02:37 +0000

    Added function to plot training histories

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -6,3 +6,12 @@ def plot_piano_roll(note_df, file_path):
     plt.rcParams["figure.figsize"] = (40,10)
     plt.imshow(note_df.T.values, cmap='hot', interpolation='nearest', aspect="auto")
     plt.savefig(file_path)
+    
+def plot_parameter_training(histories, labels, parameter='loss'):
+    for i, hist in enumerate(histories):
+        plt.plot(hist.history[parameter], label=labels[i])
+    plt.title(parameter.capitalize())
+    plt.ylabel(parameter)
+    plt.xlabel('No. epoch')
+    plt.legend(loc="upper left")
+    plt.show()
